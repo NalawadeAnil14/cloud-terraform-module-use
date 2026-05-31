be22c53525251cdab1854511d8b824848b076113
Anil Nalawade <nalawadeanil14@gmail.com>
2026-05-31 12:43:55 +0530

    Added public and private subnet cidr output

--- output.tf
+++ output.tf
@@ -17,3 +17,11 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = module.my_vpc.private_subnet_ids
 }
+
+output "public_subnet_cidrs" {
+  value = module.my_vpc.public_subnet_cidr_blocks
+}
+
+output "private_subnet_cidrs" {
+  value = module.my_vpc.private_subnet_cidr_blocks
+}
